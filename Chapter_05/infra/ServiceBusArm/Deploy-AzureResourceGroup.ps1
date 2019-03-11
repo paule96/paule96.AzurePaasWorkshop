c6fc076bee933a504b7ad9d23fcc566da1259dc3
wanu <walter.nuss@t-systems.com>
2019-03-11 23:16:32 +0100

    Changed SLN-Structure

--- Chapter_05/infra/ServiceBusArm/Deploy-AzureResourceGroup.ps1
+++ Chapter_05/infra/ServiceBusArm/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'infra',
+    [string] $ResourceGroupName = 'Chapter_05',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
